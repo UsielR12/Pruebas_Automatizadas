a34328d178664bd3f533f55f36b332b84b53d6a3
UsielR12 <76464165+UsielR12@users.noreply.github.com>
2024-07-21 01:10:00 -0600

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,41 +6,37 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-
 def send_email(subject):
     user = os.environ['EMAIL_USER']
     password = os.environ['EMAIL_PASS']
-    recipient = os.environ['RECIPIENT']
+    recipients = os.environ['RECIPIENTS'].split(",")
     body = "Adjunto el informe de las pruebas ejecutadas."
     filename = "reportprueba.html"
 
     # Crear el contenedor del mensaje
     msg = MIMEMultipart()
     msg['From'] = user
-    msg['To'] = recipient
+    msg['To'] = ", ".join(recipients)
     msg['Subject'] = subject
 
     # Adjuntar el cuerpo del mensaje
     msg.attach(MIMEText(body, 'plain'))
 
     # Adjuntar el archivo
-    attachment = open(filename, "rb")
-
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-    msg.attach(part)
+    with open(filename, "rb") as attachment:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+        msg.attach(part)
 
     # Iniciar sesión en el servidor y enviar el correo
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(user, password)
     text = msg.as_string()
-    server.sendmail(user, recipient, text)
+    server.sendmail(user, recipients, text)
     server.quit()
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
